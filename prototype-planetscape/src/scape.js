8d78102f0d1cf82f7858bf0ebb6d977036738245
Owen Swerkstrom <owenswerk@gmail.com>
2015-07-02 15:25:30 -0500

    more tweakable planetscape prototype

--- prototype-planetscape/src/scape.js
+++ prototype-planetscape/src/scape.js
@@ -1,7 +1,8 @@
 SCAPE = {
 	canv: null,
 	ctx: null,
-	ui: null,
+
+	seed: 1,
 
 	sky: "#0088ff",
 	ground: "#448800",
@@ -14,16 +15,20 @@ SCAPE = {
 };
 
 
+function random() {
+    var x = Math.sin(SCAPE.seed++) * 10000;
+    return x - Math.floor(x);
+}
 function lerp(from, to, amt) {
 	return (1 - amt) * from + amt * to;
 }
 
 function random_color() {
-	var r = Math.floor(Math.random() * 255).toString(16);
+	var r = Math.floor(random() * 255).toString(16);
 	if(r.length < 2) { r = "0" + r; }
-	var g = Math.floor(Math.random() * 255).toString(16);
+	var g = Math.floor(random() * 255).toString(16);
 	if(g.length < 2) { g = "0" + g; }
-	var b = Math.floor(Math.random() * 255).toString(16);
+	var b = Math.floor(random() * 255).toString(16);
 	if(b.length < 2) { b = "0" + b; }
 	return "#" + r + g + b;
 }
@@ -40,7 +45,7 @@ function lerp_color(col1, col2, fade) {
 	if(r.length < 2) { r = "0" + r; }
 	if(g.length < 2) { g = "0" + g; }
 	if(b.length < 2) { b = "0" + b; }
-	console.log(col1, col2, fade, "#"+r+g+b);
+//	console.log(col1, col2, fade, "#"+r+g+b);
 	return "#" + r + g + b;
 }
 
@@ -63,7 +68,8 @@ function draw_hills(horizon, color) {
 	var prevx = 0;
 	var prevy = horizon;
 	for(var i = 0; i <= SCAPE.jaggy; i++) {
-		height = horizon - (SCAPE.canv.height * (Math.random() * SCAPE.hill));
+		height = horizon - (SCAPE.canv.height * (random() * SCAPE.hill));
+		height += SCAPE.hill / 2;
 		x = SCAPE.canv.width * (i / SCAPE.jaggy);
 
 		SCAPE.ctx.lineTo(SCAPE.canv.width * (i / SCAPE.jaggy), height);
@@ -82,10 +88,10 @@ function draw_sun(radius, alpha) {
 	SCAPE.ctx.save();
 	SCAPE.ctx.fillStyle = "#ffffff";
 	SCAPE.ctx.globalAlpha = alpha;
-	console.log(radius);
+//	console.log(radius);
 	SCAPE.ctx.beginPath();
-	SCAPE.ctx.arc(Math.random() * (SCAPE.canv.width / 2),
-				  Math.random() * (SCAPE.canv.height * SCAPE.horizon),
+	SCAPE.ctx.arc(random() * (SCAPE.canv.width / 2),
+				  random() * (SCAPE.canv.height * SCAPE.horizon),
 				  radius, 0, Math.PI * 2);
 	SCAPE.ctx.closePath();
 	SCAPE.ctx.fill();
@@ -97,14 +103,14 @@ function draw() {
 
 	var i;
 
-	var sunalpha = Math.min(Math.random() + 0.5, 1);
+	var sunalpha = Math.min(random() + 0.5, 1);
 	for(i = 0; i < SCAPE.suns; i++) {
-		draw_sun(Math.random() * SCAPE.canv.height, sunalpha);
+		draw_sun(random() * SCAPE.canv.height, sunalpha);
 	}
 
 	if(SCAPE.layers) {
 		for(i = 0; i < SCAPE.layers; i++) {
-			console.log(i, i+1.0, SCAPE.layers);
+//			console.log(i, i+1.0, SCAPE.layers);
 			draw_hills(SCAPE.canv.height * SCAPE.horizon,
 //lerp(0, SCAPE.canv.height * SCAPE.horizon,
 //							(i + 1.0) / SCAPE.layers),
@@ -115,16 +121,40 @@ function draw() {
 	//console.log(SCAPE);
 }
 
+function apply() {
+	SCAPE.seed = parseInt(document.getElementsByName("seed")[0].value, null);
+	SCAPE.sky = document.getElementsByName("sky")[0].value;
+	SCAPE.ground = document.getElementsByName("ground")[0].value;
+	SCAPE.horizon = parseFloat(document.getElementsByName("horizon")[0].value,
+							   null);
+	SCAPE.layers = parseInt(document.getElementsByName("layers")[0].value,
+							null);
+	SCAPE.hill = parseFloat(document.getElementsByName("hill")[0].value, null);
+	SCAPE.jaggy = parseFloat(document.getElementsByName("jaggy")[0].value,
+							 null);
+	SCAPE.suns = parseInt(document.getElementsByName("suns")[0].value, null);
+	draw();
+}
+function setup() {
+	document.getElementsByName("seed")[0].value = SCAPE.seed;
+	document.getElementsByName("sky")[0].value = random_color();
+	document.getElementsByName("ground")[0].value = random_color();
+	document.getElementsByName("horizon")[0].value = Math.max(Math.min(random(), 0.7), 0.3);
+	document.getElementsByName("layers")[0].value = 1 + Math.floor((random() * 3));
+	document.getElementsByName("hill")[0].value = random() * 0.5;
+	document.getElementsByName("jaggy")[0].value = random() * 50;
+	document.getElementsByName("suns")[0].value = Math.floor(random() * 2) + 1;
+
+	apply();
+}
+function randomize() {
+	SCAPE.seed = new Date().getMilliseconds() * new Date().getSeconds();
+	setup();
+}
+
 window.addEventListener("load", function() {
 	SCAPE.canv = document.getElementById("display");
 	SCAPE.ctx = SCAPE.canv.getContext("2d");
-	SCAPE.ui = document.getElementById("ui");
-	SCAPE.sky = random_color();
-	SCAPE.ground = random_color();
-	SCAPE.horizon = Math.max(Math.min(Math.random(), 0.7), 0.3);
-	SCAPE.layers = 1 + Math.floor((Math.random() * 3));
-	SCAPE.hill = Math.random() * 0.5;
-	SCAPE.jaggy = Math.random() * 50;
-	SCAPE.suns = Math.floor(Math.random() * 2) + 1;
-	draw();
+
+	setup();
 });
